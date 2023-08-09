58609fdc2366fa429848058afbadd0da557a731c
ngtrhuy <trhuy180804@gmail.com>
2023-08-10 06:28:46 +0700

    thêm tính năng tìm kiếm

--- src/film/Films.js
+++ src/film/Films.js
@@ -8,6 +8,7 @@ class Films extends Component {
       cateSearch: 0,
       categories: [],
       films: [],
+      search: ''
     }
   }
 
@@ -19,16 +20,17 @@ class Films extends Component {
   }
 
   filterMoviesByCategory = () => {
-    const { films, cateSearch } = this.state;
-    if (cateSearch === 0) {
-      return films; // Không có giá trị tìm kiếm, trả về toàn bộ danh sách phim
+    const { films, cateSearch, search } = this.state;
+    //IF NOT SEARCH OR FILTER, RETURNS ALL THE FILMS
+    if (cateSearch === 0 && search === '') {
+      return films;
     }
-    
-    // Lọc danh sách phim theo giá trị cateSearch
+
     const filteredMovies = films.filter(movie =>
-      movie.cate_id === cateSearch
+      (cateSearch === 0 || movie.cate_id === cateSearch) &&
+      movie.Name.toLowerCase().includes(search.toLowerCase())
     );
-    
+
     return filteredMovies;
   };
 
@@ -75,72 +77,22 @@ class Films extends Component {
   componentDidUpdate = () => {
     console.log(this.state);
   }
-  
-  // const films = [
-  //   {
-  //     id: 1,
-  //     title: "Tập 16 vietsub",
-  //     image:
-  //       "https://simg.zalopay.com.vn/zlp-website/assets/phim_moi_chieu_rap_Alienoid_Cuoc_chien_xuyen_khong_72143fde54.jpg",
-  //     name: "Cuộc chiến xuyên không",
-  //   },
-  //   {
-  //     id: 2,
-  //     title: "Tập 14 vietsub",
-  //     image:
-  //       "https://simg.zalopay.com.vn/zlp-website/assets/phim_moi_chieu_rap_Sat_thu_doi_dau_086fc7854a.jpg",
-  //     name: "Sát thủ đối đầu",
-  //   },
-  //   {
-  //     id: 3,
-  //     title: "Tập 24 vietsub",
-  //     image:
-  //       "https://simg.zalopay.com.vn/zlp-website/assets/phim_moi_chieu_rap_Lien_minh_sieu_thu_DC_2726e64fb3.jpg",
-  //     name: "Liên mInh siêu thú",
-  //   },
-  //   {
-  //     id: 4,
-  //     title: "Tập 16 vietsub",
-  //     image:
-  //       "https://simg.zalopay.com.vn/zlp-website/assets/phim_moi_chieu_rap_Dieu_uoc_cuoi_cua_tu_nhan_2037_c80b96b25c.jpg",
-  //     name: "Điều ước của tù nhân",
-  //   },
-  //   {
-  //     id: 5,
-  //     title: "Tập 10 vietsub",
-  //     image:
-  //       "https://simg.zalopay.com.vn/zlp-website/assets/phim_moi_chieu_rap_Dan_choi_khong_so_con_roi_dafbfa57b6.jpg",
-  //     name: "Dân chơi không sợ con rơi",
-  //   },
-  //   {
-  //     id: 6,
-  //     title: "Tập 16 vietsub",
-  //     image:
-  //       "https://simg.zalopay.com.vn/zlp-website/assets/phim_moi_chieu_rap_Chuyen_ma_giang_duong_hoc_ky_2_279956df2c.jpg",
-  //     name: "Chuyện ma giảng đường",
-  //   },
-  //   {
-  //     id: 7,
-  //     title: "Tập 12 vietsub",
-  //     image:
-  //       "https://simg.zalopay.com.vn/zlp-website/assets/phim_moi_chieu_rap_Tham_tu_lung_danh_conan_nang_dau_halloween_062c584d70.jpg",
-  //     name: "Conan",
-  //   },
-  //   {
-  //     id: 8,
-  //     title: "Tập 16 vietsub",
-  //     image:
-  //       "https://simg.zalopay.com.vn/zlp-website/assets/phim_moi_chieu_rap_Alienoid_Cuoc_chien_xuyen_khong_72143fde54.jpg",
-  //     name: "Cuộc chiến xuyên không",
-  //   },
-  //   {
-  //     id: 9,
-  //     title: "Tập 14 vietsub",
-  //     image:
-  //       "https://simg.zalopay.com.vn/zlp-website/assets/phim_moi_chieu_rap_Sat_thu_doi_dau_086fc7854a.jpg",
-  //     name: "Sát thủ đối đầu",
-  //   },
-  // ];
+
+
+  handleSearchKeyPress = (event) => {
+    if (event.key === 'Enter') {
+      this.searchFilms();
+    }
+  };
+
+  handleSearch = (event) => {
+    if (event.key === 'Enter') {
+      const { value } = event.target;
+      this.setState({ search: value });
+    }
+  }
+
+
   render() {
     const filteredMovies = this.filterMoviesByCategory();
 
@@ -161,19 +113,16 @@ class Films extends Component {
               <option value="1">a-z</option>
             </select>
           </div>
-          <input className="form-control input" type="text" placeholder="Search film..." />
+          <input className="form-control input" type="text" placeholder="Search film..." onKeyPress={this.handleSearch} onChange={this.handleSearch} />
         </div>
-
-        <div>
-          <div className="wrapper-film">
-            {filteredMovies.map((item) => (
-              <div className="item-film" key={item.ID}>
-                <div className="title">{item.numberOfEpisodes} episodes</div>
-                <img className="logo" src={'./imgs/film/' + item.image} alt={item.Name} />
-                <div className="name">{item.Name}</div>
-              </div>
-            ))}
-          </div>
+        <div className="wrapper-film">
+          {filteredMovies.map((item) => (
+            <div key={item.ID} className="item-film">
+              <div className="title">{item.numberOfEpisodes} episodes</div>
+              <img className="logo" src={'./imgs/film/' + item.image} alt={item.Name} />
+              <div className="name">{item.Name}</div>
+            </div>
+          ))}
         </div>
       </div>
     );
