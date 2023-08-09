c9645ca54672ada7c77f18ca35db0890920909a6
Thanh Huyen <phamthanhhuyen19124>
2023-08-09 14:36:01 +0700

    add file footer and header

---
+++ src/Footer/Footer.js
@@ -0,0 +1,36 @@
+import "./Footer.css";
+function Footer() {
+  return (
+    <div className="footer">
+      <div className="footer-left">
+        <div className="name">StreamEase</div>
+        <div className="address">
+          <span className="active">Address</span>: 285 Doi Can, Ba Dinh, Ha Noi.
+        </div>
+        <div className="address">
+          <span className="active">Mail</span>:
+          debugTeam.job@aptechlearning.edu.vn.
+        </div>
+        <div className="address">
+          <span className="active">Phone</span>: 19008923
+        </div>
+      </div>
+      <div className="footer-right">
+        <div className="name">Our Services</div>
+        <div className="address">
+        <i class="fa-solid fa-angle-right"></i>
+          <div>Tv Show</div>
+        </div>
+        <div className="address">
+        <i class="fa-solid fa-angle-right"></i>
+          <div>Green Room</div>
+        </div>
+        <div className="address">
+        <i class="fa-solid fa-angle-right"></i>
+          <div>Film Festivals</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Footer;
