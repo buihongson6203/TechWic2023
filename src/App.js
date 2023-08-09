68f3aff13d8c96e9eca855c257681169a826e4de
long040903 <129354994+long040903@users.noreply.github.com>
2023-08-09 15:46:07 +0700

    add code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Link, Route,Routes} from 'react-router-dom'; 
+import Contact from './Contact/Contact';
+import FeedBack from './FeedBack/FeedBack';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Link to="/Contact">
+          <button>Contact</button>
+        </Link>
+        <Routes> 
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+        <Link to="/FeedBack">
+          <button>FeedBack</button>
+        </Link>
+
+        <Routes> 
+          <Route path="/FeedBack"  element={<FeedBack />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
