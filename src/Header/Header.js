e24485e085a49f3163deb71d0267704428cf4af5
khongvan-dt <138080989+khongvan-dt@users.noreply.github.com>
2023-08-10 02:17:40 +0700

    reponsive file header+siteMap van

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,40 +1,66 @@
-import "./Header.css";
+
+import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./Header.css";
+
 function Header() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const closeMobileMenu = () => {
+    setIsMobileMenuOpen(false);
+  };
+
   return (
-    <div className="header">
-      <div className="container-logo">
-        <img className="logo" src="/imgs/logo.jpeg" />
-        <div className="logo-text">StreamEase</div>
-      </div>
-      <ul className="link">
-        <li>
-          <Link to="/" className="item-link">
-            <div>Home</div>
-          </Link>
-        </li>
-        <li>
-          <Link to="/films" className="item-link">
-            <div>Films</div>
-          </Link>
-        </li>
-        <li>
-          <Link to="/feedback" className="item-link">
-            <div>Feedback</div>
-          </Link>
-        </li>
-        <li>
-          <Link to="/contact" className="item-link">
-            <div>Contact</div>
-          </Link>
-        </li>
-        <li>
-          <Link to="/sitemap" className="item-link">
-            <div>SiteMap</div>
-          </Link>
-        </li>
-      </ul>
+    <div>
+      <header className="header">
+        <nav className="navbar">
+          <div className="container-logo">
+            <img className="logo" src="/imgs/logo.jpeg" alt="Logo" />
+            <div className="logo-text">StreamEase</div>
+          </div>
+          <div
+            className={`hamburger ${isMobileMenuOpen ? "active" : ""}`}
+            onClick={toggleMobileMenu}
+          >
+            <span className="bar"></span>
+            <span className="bar"></span>
+            <span className="bar"></span>
+          </div>
+          <ul className={`nav-menu ${isMobileMenuOpen ? "active" : ""}`}>
+            <li className="nav-item">
+              <Link to="/" className="nav-link" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/films" className="nav-link" onClick={closeMobileMenu}>
+                Films
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/feedback" className="nav-link" onClick={closeMobileMenu}>
+                Feedback
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/contact" className="nav-link" onClick={closeMobileMenu}>
+                Contact
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/sitemap" className="nav-link" onClick={closeMobileMenu}>
+                SiteMap
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
     </div>
   );
 }
+
 export default Header;
