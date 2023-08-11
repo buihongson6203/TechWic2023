e076c150690b41a42274d315b3a5b48a95dc0ecf
buihongson6203 <110285987+buihongson6203@users.noreply.github.com>
2023-08-11 10:39:19 +0700

    fornt end provider

--- src/Providers/Providers.js
+++ src/Providers/Providers.js
@@ -1,64 +1,16 @@
-// // import "bootstrap/dist/css/bootstrap.min.css";
-// // import "bootstrap/dist/js/bootstrap.min.js";
-// import { useState, useEffect } from "react";
-// import "./Providers.css";
-// function providers() {
-//   const [provider, setprovider] = useState([]);
-//   useEffect(() => {
-//     axios
-//       .get("/streamingProvider.json")
-//       .then((response) => {
-//         setprovider(response.data);
-//       })
-//       .catch((error) => {
-//         console.error("Error fetching film list:", error);
-//       });
-//   }, []);
-//   return (
-//     <div className="container">
-//       <table class="table table-dark table-hover">
-//         <thead>
-//           <tr>
-//             <th scope="col">#</th>
-//             <th scope="col">Name</th>
-//             <th scope="col">Logo</th>
-//             <th scope="col">Img</th>
-//           </tr>
-//         </thead>
-//         <tbody>
-//           {provider.map((entry, index) => (
-//             <tr key={entry.ID}>
-//               <th scope="row">{entry.ID}</th>
-//               <td>{entry.Name}</td>
-//               <td>
-//                 <img src={entry.logo} alt={entry.Name} />
-//               </td>
-//               <td>
-//                 <a href={entry.url} target="_blank" rel="noopener noreferrer">
-//                   Link
-//                 </a>
-//               </td>
-//             </tr>
-//           ))}
-//         </tbody>
-//       </table>
-//     </div>
-//   );
-// }
-// export default providers;
-// Make sure to import necessary dependencies and modules
 import React, { useState, useEffect } from "react";
-import axios from "axios"; // Import axios for making API requests
+import axios from "axios";
 import "./Providers.css";
+import { FaEdit, FaTrash } from 'react-icons/fa';
 
-function Providers() { // Component names should start with an uppercase letter
-  const [providers, setProviders] = useState([]); // State should be named 'providers', not 'provider'
+function Providers() {
+  const [providers, setProviders] = useState([]);
 
   useEffect(() => {
     axios
       .get("/streamingProvider.json")
       .then((response) => {
-        setProviders(response.data); // Corrected variable name
+        setProviders(response.data);
       })
       .catch((error) => {
         console.error("Error fetching providers:", error);
@@ -67,27 +19,39 @@ function Providers() { // Component names should start with an uppercase letter
 
   return (
     <div className="container">
-      <table className="table table-dark table-hover"> {/* 'class' should be 'className' */}
-        <thead>
-          <tr>
-            <th scope="col">#</th>
-            <th scope="col">Name</th>
-            <th scope="col">Logo</th>
-            <th scope="col">Link</th> {/* Changed from 'Img' to 'Link' */}
+      <button className="btn-addnew">
+      <a className="link-addnew" href="#">Add new</a>
+      </button>
+      <table className="providers-table">
+        <thead className="status">
+          <tr >
+            <th>STT</th>
+            <th>Name</th>
+            <th>Logo</th>
+            <th>Link</th>
+            <th>Actions</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody className="titlle" >
           {providers.map((entry, index) => (
             <tr key={entry.ID}>
-              <th scope="row">{entry.ID}</th>
+              <td>{entry.ID}</td>
               <td>{entry.Name}</td>
               <td>
-              <img className="logo" src={"./imgs/film/" + entry.logo} alt={entry.Name} />
+                <img className="logo" src={"./imgs/film/" + entry.logo} alt={entry.Name} />
               </td>
               <td>
                 <a href={entry.url} target="_blank" rel="noopener noreferrer">
                   Link
                 </a>
+              </td>
+              <td className="btn-all">
+                <button type="button" className="btn btn-secondary">
+                  <FaEdit />
+                </button>
+                <button type="button" className="btn btn-secondary">
+                  <FaTrash />
+                </button>
               </td>
             </tr>
           ))}
@@ -97,4 +61,4 @@ function Providers() { // Component names should start with an uppercase letter
   );
 }
 
-export default Providers; // Make sure to export the corrected component name
+export default Providers;
