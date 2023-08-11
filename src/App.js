4013cee95edfc499b812a92b373ce652c5dbd6d9
toan10921 <33760859+toan10921@users.noreply.github.com>
2023-08-11 21:00:18 +0700

    fix fav state

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ function App() {
             <Route exact path="/sitemap" element={<SiteMap />} />
             <Route exact path="/PriceList" element={<PriceList />} />
             <Route exact path="/detail/:id" element={<DetailFilm />} />
-            <Route exact path="/fav" element={<FavFilms />} />
+            <Route exact path="/fav" element={<FavFilms  setSharedFavFilmsState={setSharedFavFilmsState} />} />
           </Routes>
 
         </div>
