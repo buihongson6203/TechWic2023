5793c37918a090764e66a2ff9095371b0125847b
long040903 <129354994+long040903@users.noreply.github.com>
2023-08-11 10:26:12 +0700

    change type swiper

--- src/home/Home.js
+++ src/home/Home.js
@@ -1,5 +1,13 @@
 import React, { useState, useEffect } from "react";
 import "./Home.css";
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+// import required modules
+import {Autoplay } from 'swiper/modules';
 import axios from "axios";
 
 function Home() {
@@ -21,6 +29,7 @@ function Home() {
   const upcomingFilms = films.filter((item) => item.status === "upcoming");
 
   return (
+    
     <div className="container-home">
       <div>
         <div className="phim-hot">Hot Movies</div>
@@ -33,6 +42,44 @@ function Home() {
             </div>
           ))}
         </div>
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={10}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 4,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 5,
+              spaceBetween: 50,
+            },
+          }}
+          modules={[Autoplay]}
+          className="mySwiper"
+        >
+          {hotFilms.map((item) => {
+           
+
+            return (
+              <SwiperSlide key={item.ID} className="item-film">
+                <div>{item.Name} </div>
+                <div><img className="logo" src={"./imgs/film/" + item.image} alt={item.Name} /></div>
+                <div className="name">{item.Name}</div>
+                
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
       </div>
       <div>
         <div className="phim-hot">New Movie Update</div>
@@ -45,6 +92,44 @@ function Home() {
             </div>
           ))}
         </div>
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={10}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 4,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 5,
+              spaceBetween: 50,
+            },
+          }}
+          modules={[Autoplay]}
+          className="mySwiper"
+        >
+          {newFilms.map((item) => {
+           
+
+            return (
+              <SwiperSlide key={item.ID} className="item-film">
+                <div>{item.Name} </div>
+                <div><img className="logo" src={"./imgs/film/" + item.image} alt={item.Name} /></div>
+                <div className="name">{item.Name}</div>
+                
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
       </div>
       <div>
         <div className="phim-hot">Upcoming Movies</div>
@@ -57,6 +142,44 @@ function Home() {
             </div>
           ))}
         </div>
+        <Swiper
+          slidesPerView={1}
+          spaceBetween={10}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          
+          breakpoints={{
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 4,
+              spaceBetween: 40,
+            },
+            1024: {
+              slidesPerView: 5,
+              spaceBetween: 50,
+            },
+          }}
+          modules={[Autoplay]}
+          className="mySwiper"
+        >
+          {upcomingFilms.map((item) => {
+           
+
+            return (
+              <SwiperSlide key={item.ID} className="item-film">
+                <div>{item.Name} </div>
+                <div><img className="logo" src={"./imgs/film/" + item.image} alt={item.Name} /></div>
+                <div className="name">{item.Name}</div>
+                
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
       </div>
     </div>
   );
