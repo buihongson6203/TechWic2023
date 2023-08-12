b8f608ea6139977aaa2def13d7840cab12f2de80
Hoang0504 <114007859+Hoang0504@users.noreply.github.com>
2023-08-12 09:46:57 +0700

    add related Movies

--- src/Favourite/fav_films.js
+++ src/Favourite/fav_films.js
@@ -3,8 +3,10 @@ import axios from 'axios';
 import "./fav_films.css";
 import * as icons from 'react-icons/fa';
 import { Link } from "react-router-dom";
-import {connect} from "react-redux";
-import {DECREMENT, INCREMENT} from "../redux/Action";
+import { connect } from "react-redux";
+import { DECREMENT, INCREMENT } from "../redux/Action";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay } from 'swiper/modules';
 
 class FavFilms extends React.Component {
     constructor(props) {
@@ -12,6 +14,7 @@ class FavFilms extends React.Component {
         this.state = {
             favFilms: [],
             films: [],
+            relatedFilms: []
         };
     }
 
@@ -34,8 +37,14 @@ class FavFilms extends React.Component {
                             film.numberOfEpisodes = numberOfEpisodes;
                         });
                         const filteredFilms = dataFilms.filter(film => favFilms.includes(film.ID));
+                        const uniqueCateIdsSet = new Set(filteredFilms.map(film => film.cate_id));
+                        const uniqueCateIdsArray = Array.from(uniqueCateIdsSet);
+                        const relatedMovies = dataFilms.filter(film => uniqueCateIdsArray.includes(film.cate_id));
 
-                        this.setState({ films: filteredFilms });
+                        this.setState({
+                            films: filteredFilms,
+                            relatedFilms: relatedMovies
+                        });
                     })
                     .catch((error) => {
                         console.log(error);
@@ -64,7 +73,7 @@ class FavFilms extends React.Component {
 
 
     render() {
-        const { films } = this.state;
+        const { films, relatedFilms } = this.state;
 
         return (
             <div className="container-film">
@@ -85,8 +94,51 @@ class FavFilms extends React.Component {
                         );
                     })}
                 </div>
+                <div className="">
+                    <h4 className="text-white mb-3">related Films</h4>
+                    <Swiper
+                        slidesPerView={1}
+                        spaceBetween={10}
+                        autoplay={{
+                            delay: 2500,
+                            disableOnInteraction: false,
+                        }}
+
+                        breakpoints={{
+                            640: {
+                                slidesPerView: 2,
+                                // spaceBetween: 20,
+                            },
+                            768: {
+                                slidesPerView: 4,
+                                // spaceBetween: 40,
+                            },
+                            1024: {
+                                slidesPerView: 5,
+                                // spaceBetween: 50,
+                            },
+                        }}
+                        modules={[Autoplay]}
+                        className="mySwiper"
+                    >
+                        {relatedFilms.map((item) => {
+                            return (
+                                <SwiperSlide key={item.ID} className="item-film">
+                                    <div className="item-inner poisiton-relative">
+                                        <div className="tap">{item.numberOfEpisodes} episodes</div>
+                                        <Link to={`/detail/${item.ID}`} className="item-link">
+                                            <img className="logo-slider" src={'./imgs/film/' + item.image} alt={item.Name} />
+                                        </Link>
+                                        <div className="name">{item.Name} <strong className="red">({item.streamingProvider})</strong></div>
+                                    </div>
+                                </SwiperSlide>
+                            );
+                        })}
+                    </Swiper>
+                </div>
+
             </div>
         );
     }
 }
-export default connect(null, {  DECREMENT })(FavFilms)
+export default connect(null, { DECREMENT })(FavFilms)
