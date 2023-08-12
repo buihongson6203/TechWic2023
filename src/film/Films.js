d17a52ab9964e1f9927c84efdfcbcb0f550ccf7a
Hoang0504 <114007859+Hoang0504@users.noreply.github.com>
2023-08-12 10:32:02 +0700

    change

--- src/film/Films.js
+++ src/film/Films.js
@@ -334,26 +334,26 @@ class Films extends Component {
           <Swiper
             slidesPerView={1}
             spaceBetween={10}
-            // autoplay={{
-            //   delay: 2500,
-            //   disableOnInteraction: false,
-            // }}
+            autoplay={{
+              delay: 2500,
+              disableOnInteraction: false,
+            }}
 
             breakpoints={{
               640: {
                 slidesPerView: 2,
-                // spaceBetween: 20,
+                spaceBetween: 20,
               },
               768: {
                 slidesPerView: 4,
-                // spaceBetween: 40,
+                spaceBetween: 40,
               },
               1024: {
                 slidesPerView: 5,
-                // spaceBetween: 50,
+                spaceBetween: 50,
               },
             }}
-            // modules={[Autoplay]}
+            modules={[Autoplay]}
             className="mySwiper"
           >
             {filteredMovies.map((item) => {
